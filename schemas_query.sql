f14831d50bd630f59197c4a5a4ee1b0c4d27a87a
sjeter1016 <119820731+sjeter1016@users.noreply.github.com>
2023-06-08 23:25:04 -0400

    Add files via upload

---
+++ schemas_query.sql
@@ -0,0 +1,53 @@
+CREATE TABLE titles(
+	title_id VARCHAR  NOT NULL,
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+
+CREATE TABLE employees(
+	 emp_no INT  NOT NULL,
+	 emp_title_id VARCHAR NOT NULL,
+	 birth_date Date NOT NULL,
+	 first_name VARCHAR NOT NULL,
+	 last_name VARCHAR NOT NULL,
+	 sex VARCHAR NOT NULL,
+	 hire_date DATE NOT NULL,
+	 PRIMARY KEY (emp_no),
+	 FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
+
+
+
+CREATE TABLE departments(
+	 dept_no VARCHAR NOT NULL,
+	 dept_name VARCHAR NOT NULL,
+	 PRIMARY KEY (dept_no)
+);	
+	
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no), 
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);	
+
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no, emp_no), 
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+
+
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
